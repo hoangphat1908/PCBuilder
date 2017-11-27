47d4773c97e454d04fa33bd08a09896f5f180d78
Jay Lam <lamjay415@gmail.com>
2017-11-26 22:38:12 -0800

    populated table data

--- src/main/mysql/data.sql
+++ src/main/mysql/data.sql
@@ -21,76 +21,146 @@ SET FOREIGN_KEY_CHECKS=1;
 
 INSERT INTO `cpu` (manufacturer, model, price, cores, clock_speed, TDP)
 VALUES
-    ('Intel', 'Core i7-7700K', 289.99, 4, 4.2, 91),
+    ('Intel', 'Core i7-7700K', 318.34, 4, 4.2, 91),
     ('AMD', 'Ryzen 5 1600', 193.44, 6, 3.2, 65),
     ('Intel', 'Core i7-8700K', 414.89, 6, 3.7, 65),
-    ('AMD','FX-6300',79.99,6,3.5,95),
-    ('Intel','Core i5-7600K',249.99,4,3.8,91),
-    ('AMD','Ryzen 7 1700X',279.99,8,3.4,95),
-    ('Intel','Core i3-7100K',109.99,2,3.9,51),
-    ('AMD','Ryzen 5 1600X',199.99,6,3.6,95),
-   	('Intel','Core i5-7500',179.99,4,3.4,65),
-   	('AMD','Ryzen 7 1700',269.99,8,3.0,65),
-    ('Intel', 'Core i7-7700',304.99,4,3.6,65),
-    ('AMD','Ryzen 3 1200',99.99,4,3.1,65),
-    ('Intel',' Core i7-7820X',499.99,8,3.6,140),
-    ('AMD','Ryzen Threadripper 1950X',799.99,16,3.4,180),
-    ('Intel',' Core i7-7800X',309.99,6,3.5,140),
-    ('AMD','Ryzen 5 1500X',164.99,4,3.5,65),
-	('Intel',' Core i7-6700',325.37,4,3.4,65);
-	
+    ('Intel', 'Core i7-6700K', 299.99, 4, 4, 91),
+    ('Intel', 'Core i7-3770K', 554.05, 4, 3.5, 77),
+    ('Intel', 'Core i7-4790K', 333.99, 4, 4, 88),
+    ('Intel', 'Core i7-5930K', 557.99, 6, 3.5, 140),
+    ('Intel', 'Core i5-3570K', 275, 4, 3.4, 77),
+    ('Intel', 'Core i5-6600K', 228.59, 4, 3.5, 91),
+    ('Intel', 'Xeon E3-1231 V3', 329.87, 4,3.4,80),
+    ('Intel', 'Core i7-4770K', 455.97, 4,3.5,84),
+    ('Intel', 'Core i5-6500', 189.58, 4, 3.2,65),
+    ('Intel', 'Core i5-4670K', 309.39, 4, 3.4, 84),
+    ('Intel', 'Core i5-7600K', 199.89, 4, 3.8, 91),
+    ('AMD', 'Ryzen 5 1600X', 199.99, 6, 3.6, 95);
+
 INSERT INTO `motherboard` (manufacturer, model, price, form_factor, socket, ram_slots, ram_max)
 VALUES
     ('MSI', 'Z270-A PRO', 89.88, 'ATX', 'LGA1151', 4, 64),
     ('Asus', 'STRIX Z270-E GAMING', 189.88, 'ATX', 'LGA1151', 4, 64),
     ('MSI', 'B250M PRO-VD', 52.39, 'Micro ATX', 'LGA1151', 2, 32),
-    ('MSI','Z97 PC Mate',187.94,'ATX','LGA 1150',4,32),
-    ('MSI','H97 PC Mate',171.97,'ATX','LGA 1150',4,32),
-    ('MSI','Z97S SLI Krait Edition',199.99,'ATX','LGA 1150',4,32),
-    ('Gigabyte','Z370 AORUS GAMING 7',199.99,'ATX','LGA1151',4,64),
-    ('Asus','ROG STRIX B350-F GAMING',129.99,'ATX','AM4',4,64),
-    ('ASRock','AB350M Pro4',59.99,'ATX','AM4',4,64),
-    ('Gigabyte','GA-AB350-GAMING 3',84.99,'ATX','AM4',4,64),
-    ('Gigabyte','GA-970A-DS3P FX',58.99,'ATX','AM3+',4,32),
-    ('ASRock','X370 Taichi',184.99,'ATX','AM4',4,64),
-    ('Asus','PRIME B350-PLUS',79.99,'ATX','AM4',4,64),
-    ('Asus','ROG Maximus X Hero',279.99,'ATX','LGA 1151',4,64),
-    ('ASRock','AB350 GAMING-ITX/ac',94.99,'ITX','AM4',4,32),
-    ('ASRock','Z270 Extreme 4',134.99,'ATX','LGA 1151',4,64);
+    ('Gigabyte', 'Z370 AORUS GAMING WIFI (rev. 1.0)', 99.99, 'ATX', 'LGA1151',4,64),
+    ('Asus', 'ROG RAMPAGE VI EXTREME', 649.99, 'EATX', 'LGA2066', 8, 128),
+    ('Gigabyte', 'A-970A-DS3P FX (rev. 2.1)', 48.99, 'ATX', 'AM3+/AM3', 4, 32),
+    ('MSI', 'Z370-A PRO', 101.98, 'ATX', 'LGA1151', 4,64),
+    ('Asus', 'ROG STRIX Z370-G GAMING (WI-FI AC)', 176.99, 'Micro ATX', 'LGA1151', 4, 64),
+    ('Asus', 'ROG STRIX Z370-F GAMING', 191.99, 'ATX', 'LGA1151', 4, 64),
+    ('Gigabyte', 'Z370 HD3 (rev. 1.0)', 101.49, 'ATX', 'LGA1151', 4, 64),
+    ('Gigabyte', 'Z370 AORUS Ultra Gaming (rev. 1.0)', 119.99, 'ATX', 'LGA1151', 4, 64),
+	('Asus', 'STRIX B250H Gaming', 69.99, 'ATX', 'LGA1151', 4, 64),
+    ('MSI', 'Z370 GODLIKE GAMING', 484.99, 'EATX', 'LGA1151', 4, 64),
+    ('Asus', 'Prime Z370-P', 138.89, 'ATX', 'LGA1151', 4, 64),
+    ('MSI', 'A320M PRO-VH PLUS', 41.99, 'Micro ATX', 'AM4', 2, 32);
     
 INSERT INTO `gpu` (manufacturer, model, price, series, chipset, memory, core_clock)
 VALUES
     ('MSI', 'GTX 1050 Ti GAMING X 4G', 174.99, 'GAMING X 4G', 'GeForce GTX 1050 Ti', 4, 1.29),
     ('Asus', 'STRIX-GTX1080TI-11G-GAMING', 749.99, 'STRIX GAMING', 'GeForce GTX 1080 Ti', 11, 1.49),
-    ('Asus', 'ROG STRIX-GTX1080-A8G-GAMING', 554.98, 'ROG STRIX', 'GeForce GTX 1080', 8, 1.67);
-    
+    ('Asus', 'ROG STRIX-GTX1080-A8G-GAMING', 554.98, 'ROG STRIX', 'GeForce GTX 1080', 8, 1.67),
+    ('EVGA', '03G-P4-2884-KR', 289.99, 'Superclocked ACX', 'GeForce GTX 780', 3, 0.967),
+    ('EVGA', '08G-P4-6173-KR', 457.89, 'SC Gaming ACX 3.0', 'GeForce GTX 1070', 8, 1.59),
+    ('EVGA', '03G-P4-2883-KR', 279.99, 'Superclocked', 'GeForce GTX 780 Ti', 3, 0.98),
+    ('EVGA', 'EVGA 04G-P4-2983-KR', 377.39, 'Superclocked ACX 2.0', 'GeForce GTX 980', 4, 1.27),
+    ('EVGA', 'EVGA 02G-P4-2774-KR', 169.99, 'Superclocked ACX', 'GeForce GTX 770', 2, 1.11),
+    ('MSI', 'GTX 1070 GAMING X 8G', 494.99, 'GAMING X 8G', 'GeForce GTX 1070', 8, 1.51),
+    ('MSI', 'GTX 980 Ti GAMING 6G', 469.99, 'GAMING X 6G', 'GeForce GTX 980 Ti', 6, 1.18),
+    ('ASUS', 'ROG STSRIX-GTX1070-O8G-GAMING', 444.98, 'ROG STRIX', 'GeForce 1070', 8, 1.63),
+    ('EVGA', 'EVGA 04G-P4-6253-KR', 156.99, 'SC GAMING ACX 2.0', 'GeForce 1050 Ti', 4, 1.35),
+    ('MSI', 'MSI GTX 1070 GAMING X 8G', 469.88, 'GAMING X 8G', 'GeForce 1070', 8, 1.51),
+    ('ASUS', 'STRIX-GTX1080TI-O11G-GAMING', 804.98, 'STRIX GAMING', 'GeForce 1080 Ti', 11, 1.57),
+    ('Gigabyte' 'GV-N1080WF3OC-8GD', 499.99, 'WINDFORCE OC8G', 'GeForce GTX 1080', 8, 1.66);
     
 INSERT INTO `psu` (manufacturer, model, price, series, form_factor, watts, efficiency, modular)
 VALUES
     ('EVGA', 'SuperNOVA 750', 107.89, 'SuperNOVA G2', 'ATX', '750W', '80+ Gold', 'Full'),
     ('Corsair', 'CX550M', 59.99, 'CXM', 'ATX', '550W', '80+ Bronze', 'Semi'),
-    ('EVGA', 'SuperNOVA 650', 77.99, 'SuperNOVA NEX', 'ATX', '650W', '80+ Gold', 'No');
-   
+    ('EVGA', 'SuperNOVA 650', 77.99, 'SuperNOVA NEX', 'ATX', '650W', '80+ Gold', 'No'),
+    ('EVGA', 'SuperNOVA 850', 93.98, 'SuperNOVA G2', 'ATX', '850W', '80+ Gold', 'Full'),
+    ('EVGA', '210-GQ-0650-V1', 71.89, '', 'ATX', '650W', '80+ Gold', 'Semi'),
+    ('EVGA', 'SuperNOVA 550', 74.89, 'SuperNOVA G2', 'ATX', '550W', '80+ Gold', 'Full'),
+    ('Corsair', 'AX760', 159.88, '', 'ATX', '760W', '80+ Platinum', 'Full'),
+    ('Corsair', 'AX1200i', 299.99, '', 'ATX', '1200W', '80+ Platinum', 'Full'),
+    ('Corsair', 'AX750', 199.99, 'Professional Gold', 'ATX', '750W', '80+ Gold', 'Full'),
+    ('Corsair', 'AX860', 155.98, '', 'ATX', '860W', '80+ Platinum', 'Full'),
+    ('Corsair', 'HX1000i', 199.99, '', 'ATX', '1000W', '80+ Platinum', 'Full'),
+    ('SeaSonic', 'Platinum-1000', 169.99, 'Platinum', 'ATX', '1000W', '80+ Platinum', 'Full'),
+    ('Corsair', 'AX850', 229.99, 'Professional Gold', 'ATX', '850W', '80+ Platinum', 'Full'),
+    ('Corsair', 'RM650x', 69.98, 'RMx', 'ATX', '650W', '80+ Gold', 'Full'),
+    ('XFX', 'P1-750B-BEFX', 220.64, 'XTR', 'ATX', '750W', '80+ Gold', 'Full');
+
 INSERT INTO `ram` (manufacturer, model, price, size, module, type, speed)
 VALUES
     ('G.Skill', 'NT Series', 83.88, 8, '1x8GB', '288-pin DIMM', 'DDR4-2400'),
     ('G.Skill', 'Ripjaws V Series', 169.95, 16, '2x8GB', '288-pin DIMM', 'DDR4-2666'),
-    ('Corsair', 'Vengeance LPX', 188.99, 16, '2x8GB', '288-pin DIMM', 'DDR4-3000');
+    ('Corsair', 'Vengeance LPX', 188.99, 16, '2x8GB', '288-pin DIMM', 'DDR4-3000'),
+    ('G.Skill', 'Ripjaws Series', 74.49, 8, '2x4GB', '240-pin DIMM', 'DDR3-1600'),
+    ('Kingston', 'HyperX Fury Black', 117.94, 8, '1x8GB', '288-pin DIMM', 'DDR4-2133'),
+    ('Kingston', 'HyperX Fury Black', 143.04, 16, '2x8GB', '240-pin DIMM', 'DDR3-1866'),
+    ('Corsair', 'Vengeance LPX', 194.88, 16, '2x8GB', '288-pin DIMM', 'DDR4-3000'),
+    ('Kingston', 'HyperX Fury Black', 109.99, 8, '2x4GB', '288-pin DIMM', 'DDR4-2133'),
+    ('Kingston', 'HyperX Fury Black', 213.78, 16, '2x8GB', '288-pin DIMM', 'DDR4-2133'),
+    ('G.Skill', 'Ripjaws X Series', 68.99, 8, '2x4GB', '240-pin DIMM', 'DDR3-1600'),
+    ('G.Skill', 'Sniper', 78.99, 8, '2x4GB', '240-pin DIMM', 'DDR3-1600'),
+    ('G.Skill', 'Ripjaws X Series', 177.99, 16, '2x8GB', '240-pin DIMM', 'DDR3-1866'),
+    ('G.Skill', 'Ares Series', 68.99, 8, '2x4GB', '240-pin DIMM', 'DDR3-1600'),
+    ('G.Skill', 'Ripjaws X Series', 93.99, 8, '2x4GB', '240-pin DIMM', 'DDR3-2133'),
+    ('G.Skill', 'Ripjaws X Series', 92.99, 8, '2x4GB', '240-pin DIMM', 'DDR3-1866');
+    
  
 INSERT INTO `storage` (manufacturer, model, price, type, form, series, capacity, cache)
 VALUES
     ('Western Digital', 'WD10EZEX', 41.77, '7200RPM', '3.5"', 'Caviar Blue', '1TB', '64MB'),
     ('Samsung', 'MZ-75E250B/AM', 94.99, 'SSD', '2.5"', '850 EVO-Series', '250GB', NULL),
-    ('Samsung', 'MZ-75E500B/AM', 149.99, 'SSD', '2.5"', '850 EVO-Series', '500GB', NULL);
+    ('Samsung', 'MZ-75E500B/AM', 149.99, 'SSD', '2.5"', '850 EVO-Series', '500GB', NULL),
+    ('Samsung', 'MZ-7PD256BW', 297.38, 'SSD', '2.5"', '840 Pro Series', '256GB', NULL),
+    ('Samsung', 'MZ-7TE120BW', 169.99, 'SSD', '2.5"', '840 EVO-Series', '120GB', NULL),
+    ('Samsung', 'MZ-7KE256BW', 109.99, 'SSD', '2.5"', '850 Pro Series', '256GB', NULL),
+    ('Samsung', 'MZ-V6E1T0', 459.99, 'SSD', 'M.2-2280', '960 EVO-Series', '1TB', '1000MB'),
+    ('Samsung', 'MZ-7PD128BW', 279.99, 'SSD', '2.5"', '840 Pro Series', '128GB', NULL),
+    ('Samsung', 'MZ-V6E500', 247.88, 'SSD', 'M.2-2280', '960 EVO-Series', '500GB', '512MB'),
+    ('Samsung', 'MZ-7TE250BW', 149.88, 'SSD', '2.5"', '840 EVO-Series', '250GB', NULL),
+    ('Samsung', 'MZ-V6E250', 127.89, 'SSD', 'M.2-2280', '960 EVO-Series', '250GB', '512MB'),
+    ('Crucial', 'CT512MX100SSD1', 299.99, 'SSD', '2.5"', 'MX100', '512GB', NULL),
+    ('Samsung', 'MZ-75E1T0B/AM', 279.99, 'SSD', '2.5"', '850 EVO-Series', '1TB', NULL),
+    ('Samsung', 'MZ-7PC128B/WW', 199.99, 'SSD', '2.5"', '830 EVO-Series', '128GB', NULL),
+    ('PNY', 'SSD7CS1311-240-RB', 97.49, 'SSD', '2.5"', 'CS1311', '240GB', NULL);
     
 INSERT INTO `cooler` (manufacturer, model, price, rpm, noise)
 VALUES
     ('Cooler Master', 'Hyper 212 EVO', 24.39, '600 - 2000 RPM', '9.0 - 36.0 dbA'),
     ('Corsair', 'H100i v2', 99.99, '2435 RPM', '37.7 dbA'),
-    ('NZXT', 'Kraken X62 Rev 2', 414.89, '500 - 1800 RPM', '21.0 - 38.0 dbA');
+    ('NZXT', 'Kraken X62 Rev 2', 414.89, '500 - 1800 RPM', '21.0 - 38.0 dbA'),
+    ('Noctua', 'NH-D15', 88.88, '300 - 1500 RPM', '19.2 - 24.6 dbA'),
+    ('Noctua', 'NH-U9S', 58.35, '400 - 2000 RPM', '16.3 - 22.8 dbA'),
+    ('Noctua', 'NH-D15', 74.95, '900 - 1200 RPM', '12.6 - 19.8 dbA'),
+    ('Noctua', 'NH-U12S', 57.99, '300 - 1500 RPM', '18.6 - 22.4 dbA'),
+    ('CRYORIG', 'H7', 28.9, '330 - 1600 RPM', '1.0 - 25.0 dbA'),
+    ('CRYORIG', 'M9i', 18.99, '600 - 2200 RPM', '26.4 dbA'),
+    ('CRYORIG', 'H5 Ultimate', 42.89, '700 - 1300 RPM', '19.0 - 23.0 dbA'),
+    ('CRYORIG', 'H100i', 99.99, '2700 RPM', '37.7 dbA'),
+    ('be quiet!', 'Pure Rock Slim', 21.49, '2000 RPM', '13.1 - 25 dbA'),
+    ('Corsair', 'H115i', 109.99, '2000 RPM', '40 dbA'),
+    ('Corsair', 'H105', 89.99, '800 - 2700 RPM', '37.7 dbA'),
+    ('be quiet!', 'Dark Rock Pro 3', 74.49, '1400 - 1700 RPM', '13.2 - 26.1 dbA');
+    
     
 INSERT INTO `computer_case` (manufacturer, model, price, type, psu_included)
 VALUES
     ('NZXT', 'S340 (White)', 61.99, 'ATX Mid Tower', NULL),
     ('Rosewill', 'FBM-01', 21.99, 'MicroATX Mini Tower', NULL),
-    ('EVGA', 'Hadron', 169.99, 'Mini ITX Tower', '500W');
+    ('EVGA', 'Hadron', 169.99, 'Mini ITX Tower', '500W'),
+    ('Corsair', '200R', 39.99, 'ATX Mid Tower', NULL),
+    ('Raidmax', 'Vortex', 35.98, 'ATX Mid Tower', NULL),
+    ('Corsair', '750D', 99.99, 'ATX Mid Tower', NULL),
+    ('Corsair', 'Crystal 570X RGB', 139.98, 'ATX Mid Tower', NULL),
+    ('Deepcool', 'TESSERACT SW', 36.99, 'ATX Mid Tower', NULL),
+    ('Thermaltake', 'Core V21', 34.99, 'ATX Mid Tower', NULL),
+    ('Cooler Master', 'MasterBox Lite 5 RGB', 76.98, 'ATX Mid Tower', NULL),
+    ('Cooler Master', 'MasterBox Lite 5', 44.99, 'ATX Mid Tower', NULL),
+    ('Fractal Design', 'Focus G (Black)', 39.99, 'ATX Mid Tower', NULL),
+    ('Fractal Design', 'Focus G (White)', 54.99, 'ATX Mid Tower', NULL),
+    ('NZXT', 'S340 (Black/Blue)', 49.99, 'ATX Mid Tower', NULL),
+    ('Corsair', '760T Black', 149.99, 'ATX Mid Tower', NULL);
